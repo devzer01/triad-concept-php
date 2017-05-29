3a8f0b173babfb7afd741f0de5835abd62409556
devzer01 <V8UAEuP09X>
2017-05-29 11:32:09 +0700

    fix channel

--- modules/channels.php
+++ modules/channels.php
@@ -1,3 +1,18 @@
 <?php
+$id = $_GET['id'];
+$result = [];
+$channel = DBconnect::assoc_query_2D("SELECT * FROM channel WHERE id = :id ORDER BY ts DESC", [':id' => $id]);
+$msgs = DBconnect::assoc_query_2D("SELECT message FROM message WHERE topic_id = :id ORDER BY ts DESC", [':id' => $id]);
+
+$messages = [];
+foreach ($msgs as $msg) {
+    $messages = ['msg' => $msg['message'], 'sender' => $sender ];
+}
+
+$smarty->assign('channel', $channel);
+$smarty->assign('messages', $messages);
+
+$channel = 0;
+
 
 $smarty->display('channels.tpl');
