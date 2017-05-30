0996d81bf81cde7b76181e049f72c657cf224eb1
devzer01 <V8UAEuP09X>
2017-05-30 19:26:24 +0700

    remove unwated files

--- classes/top.class.php
+++ classes/top.class.php
@@ -18,27 +18,7 @@ $_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
 $_SESSION['request'] = $_SERVER['REQUEST_URI'];
 
 //require classes and libs//
-require_once('sites/orowo.oro.world/configs/ger.php');
-require_once('classes/funcs.php');
-require_once('classes/funcs2.php');
-require_once(SITE.'sms.php');
-require_once('classes/search.class.php');
-require_once('Mail.php');
-require_once('Mail/mime.php');
-require_once('libs/SmartyPaginate.class.php');
 require_once('libs/SmartyBC.class.php');
-require_once('classes/class.phpmailer.php');
-
-$_SESSION['sess_externuser'] = 1;
-
-if(!isset($_SESSION['sess_id']))
-{
-	if(isset($_COOKIE['username']) && isset($_COOKIE['password']))
-	{
-		funcs::checkCookie();
-	}
-}
-
 $smarty = new SmartyBC();	//call smarty class
 
 $smarty->setCacheDir('sites/orowo.oro.world/cache');
